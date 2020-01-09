0085ca9ce844b29dd46645d88af4ee8d7efcd422
Louponius <59686499+Louponius@users.noreply.github.com>
2020-01-09 10:40:11 +0100

    Create Robot.js

---
+++ Robot.js
@@ -0,0 +1,22 @@
+function Robot(cmd,arg1,arg2,arg3,arg4){
+  if (cmd == "Beep"){
+    console.log(arg1)
+    return arg1
+  }
+
+  if (cmd == "Boop"){
+    if (arg1 > arg2){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if (cmd == "Bap"){
+     if (arg1 < arg2){
+        return true
+      } else {
+        return false
+      }
+   }
+}
